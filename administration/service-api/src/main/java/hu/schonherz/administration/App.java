2263761bd91e09c4cdf2a2fa674b45077cc1ef02
kosimiki <kosimiki@gmail.com>
2015-12-01 14:05:55 +0100

    added project

---
+++ administration/service-api/src/main/java/hu/schonherz/administration/App.java
@@ -0,0 +1,13 @@
+package hu.schonherz.administration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
