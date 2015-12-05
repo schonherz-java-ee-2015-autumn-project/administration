0c6a011bc5c97ed55e8b3c079c85270cbedf318f
kosimiki <kosimiki@gmail.com>
2015-12-05 20:55:08 +0100

    user service added

---
+++ administration/service-api/src/main/java/hu/schonherz/administration/service/UserService.java
@@ -0,0 +1,24 @@
+package hu.schonherz.administration.service;
+
+import java.util.List;
+
+import	hu.schonherz.administration.service.dto.UserDTO;
+
+public interface UserService {
+
+	public UserDTO findUserByName(String name) throws Exception;
+
+	public UserDTO registrationUser(UserDTO UserDTO) throws Exception;
+
+	public List<UserDTO> getUserList(int i, int pageSize, String sortField, int dir, String filter,
+			String filterColumnName);
+
+	public List<UserDTO> getUsers();
+	
+	public Integer getUserCount();
+
+	public UserDTO saveUser(UserDTO selectedUser);
+
+	public UserDTO findById(Long id);
+
+}
