342bfca73b1894d49dc72b73ecf9ce4ec3bb10c0
Viktor Pusinszky <luxeon@hotmail.hu>
2015-12-22 14:37:49 +0100

    admin modify page work

--- administration/web/src/main/java/hu/schonherz/administration/web/admin/AdminBean.java
+++ administration/web/src/main/java/hu/schonherz/administration/web/admin/AdminBean.java
@@ -1,22 +1,58 @@
 package hu.schonherz.administration.web.admin;
 
+import java.io.IOException;
+
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.SelectEvent;
 
-@Named("adminLazyBean")
+import hu.schonherz.administration.serviceapi.dto.UserDTO;
+
+
+@Named("adminBean")
 @ViewScoped
 public class AdminBean {
-
+	
 	@Inject
 	private LazyAdmin lazyDataModel;
+	
+	private UserDTO selected;
+	
+	public void redirect(SelectEvent event) {
+		if(!selected.equals(((UserDTO)event.getObject()))){
+			selected = (UserDTO)event.getObject();
+		}
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+	    try {
+			ec.redirect(ec.getRequestContextPath() + "/secured/administration/edit.xhtml?selected="+selected.getId());
+		} catch (IOException e) {
+		}
+    
+	}
+	
+	
+	public UserDTO getSelected() {
+		return selected;
+	}
+
+
+	public void setSelected(UserDTO selected) {
+		this.selected = selected;
+	}
+
+
 	public LazyAdmin getLazyDataModel() {
 		return lazyDataModel;
 	}
+	
 	public void setLazyDataModel(LazyAdmin lazyDataModel) {
 		this.lazyDataModel = lazyDataModel;
 	}
+	
 	
 
 
--- administration/web/src/main/java/hu/schonherz/administration/web/admin/AdminModifyBean.java
+++ administration/web/src/main/java/hu/schonherz/administration/web/admin/AdminModifyBean.java
@@ -1,81 +1,66 @@
 package hu.schonherz.administration.web.admin;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
 
 import hu.schonherz.administration.serviceapi.UserService;
+import hu.schonherz.administration.serviceapi.dto.UserDTO;
+import hu.schonherz.administration.web.localization.MessageProvider;
 
-@ManagedBean(name = "AdminModifyBean")
+@Named("adminEditBean")
+@ViewScoped
+@EJB(name = "ejb.UserService", beanInterface = UserService.class)
 public class AdminModifyBean {
 	
 	@EJB
 	private UserService userService;
-	
-	
-	@ManagedProperty(value = "#{param.userID}")
+	private UserDTO selected;
 	private long id;
 	
-	@ManagedProperty(value = "#{param.name}")
-	private String name;
-	
-	@ManagedProperty(value = "#{param.userName}")
-	private String userName;
-	
-	@ManagedProperty(value = "#{param.phoneNumber}")
-	private String phoneNumber;
-	
-	
-	
+	public void modify() {
+			try {
+				userService.saveUser(selected);
+				FacesContext context = FacesContext.getCurrentInstance();
+				FacesMessage message = new FacesMessage(MessageProvider.getValue("successful_edit"));
+				message.setSeverity(FacesMessage.SEVERITY_INFO);
+				context.addMessage("adminForm:save_status", message);
+
+			} catch (Exception e) {
+				FacesContext context = FacesContext.getCurrentInstance();
+				context.addMessage("adminForm:save_status",
+						new FacesMessage(MessageProvider.getValue("edit_failed")));
+			}
+	}
+	public void init() {
+		selected = userService.findById(id);
+	}
 	public UserService getUserService() {
 		return userService;
 	}
+
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-	public void setId(long id) {
-		this.id = id;
+
+	public UserDTO getSelected() {
+		return selected;
 	}
+
+	public void setSelected(UserDTO selected) {
+		this.selected = selected;
+	}
+
 	public long getId() {
 		return id;
 	}
-	
-	
-	
-	
-	public String getName() {
-		return name;
+
+	public void setId(long id) {
+		this.id = id;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	
-	public void setUserData(){
-		
-		try {
-			
-			userService.setUserData(id);
-			
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-	}
-	
-	
 	
 	
 }
