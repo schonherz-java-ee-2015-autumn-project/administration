c774a6a3f1829746e1bebab84587e6544591bf9a
ErikKakocz <erik.kakocz@gmail.com>
2015-12-22 21:30:02 +0100

    regadmin teszt roud 3

--- administration/web/src/main/java/hu/schonherz/administration/beans/RegistrationBean.java
+++ administration/web/src/main/java/hu/schonherz/administration/beans/RegistrationBean.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import hu.schonherz.administration.serviceapi.UserService;
 import hu.schonherz.administration.serviceapi.dto.UserDTO;
 
-
 @RequestScoped
 @ManagedBean
 @EJB(name = "ejb.UserService", beanInterface = UserService.class)
@@ -24,9 +23,6 @@ public class RegistrationBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-
-	
 
 	@EJB
 	UserService userService;
@@ -38,18 +34,20 @@ public class RegistrationBean implements Serializable {
 	String passconf;
 	FacesMessage msg;
 	FacesContext current;
-	
+
 	public void registration() {
 		UIViewRoot view = FacesContext.getCurrentInstance().getViewRoot();
 		current = FacesContext.getCurrentInstance();
 		UserDTO user = new UserDTO();
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		if (!isValid()) {
-			
-			//msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "A két jelszónak meg kell egyeznie!", "A két jelszónak meg kell egyeznie!");
-			//current.addMessage(null, msg);
+
+			// msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "A két
+			// jelszónak meg kell egyeznie!", "A két jelszónak meg kell
+			// egyeznie!");
+			// current.addMessage(null, msg);
 		} else {
-			
+
 			user.setName(name);
 			user.setUsername(username);
 			user.setPhoneNumber(phone);
@@ -57,95 +55,82 @@ public class RegistrationBean implements Serializable {
 			try {
 				userService.registrationAdmin(user);
 			} catch (Exception e) {
-				
-				
+
 				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Hiba a regisztráció közben!", "Hiba a regisztráció közben.");
 				current.addMessage(null, msg);
 				e.printStackTrace();
 			}
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sikeres regisztráció!", "Sikeres regisztráció!");
 			current.addMessage(null, msg);
-			name=username=phone=null;
+			name = username = phone = null;
 		}
 	}
 
-	public boolean isValid(){
-		return isNameValid() &&
-				isUserNameValid() &&
-				isPhoneValid() &&
-				isPassValid() &&  
-			    arePasswordsEqual();
-		
+	public boolean isValid() {
+		return isNameValid() && isUserNameValid() && isPhoneValid() && isPassValid() && arePasswordsEqual();
+
 	}
-	
+
 	private boolean isNameValid() {
-		if(name.length()>=3 && name.length()<=150 && name.matches("[' 'a-zöüóőúéáűíA-ZÖÜÓŐÚÉÁŰÍ/-]*"))
+		if (name.length() >= 3 && name.length() <= 150 && name.matches("[' 'a-zöüóőúéáűíA-ZÖÜÓŐÚÉÁŰÍ/-]*"))
 			return true;
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Érvénytelen név", "Érvénytelen név");
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}","#{out.nameError}");
 			current.addMessage(null, msg);
 			return false;
 		}
 	}
-	
+
 	private boolean isUserNameValid() {
-		
-		UserDTO user=null;
+
+		UserDTO user = null;
 		try {
 			user = userService.findUserByName(username);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			
-		}
-		if(user==null)
-		if( username.length()>=6 && 
-			   username.length()<=16 && 
-			   username.matches("[0-9a-zöüóőúéáűíA-ZÖÜÓŐÚÉÁŰÍ/_]*"))
-			return true;
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Érvénytelen felhasználónév", "Érvénytelen felhasználónév");
-			current.addMessage(null, msg);
-			return false;
-		}
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "A Felhasználónév foglalt", "A Felhasználónév foglalt");
+
+		}	
+		if (user == null)
+			if (username.length() >= 6 && username.length() <= 16 && username.matches("[0-9a-zöüóőúéáűíA-ZÖÜÓŐÚÉÁŰÍ/_]*"))
+				return true;
+			else {
+				msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}", "#{out.usernameError}");
+				current.addMessage(null, msg);
+				return false;
+			}
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}", "#{out.takenNameError}");
 			current.addMessage(null, msg);
 			return false;
 		}
 	}
 
 	private boolean isPhoneValid() {
-		if( phone.length()>=7 &&
-			   phone.length()<=11 &&
-			   phone.matches("[0-9]+"))
+		if (phone.length() >= 7 && phone.length() <= 11 && phone.matches("[0-9]+"))
 			return true;
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Érvénytelen telefonszám", "Érvénytelen telefonszám");
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}", "#{out.phoneError}");
 			current.addMessage(null, msg);
 			return false;
 		}
-		
+
 	}
 
 	private boolean isPassValid() {
-		if( password.length()>=8 &&
-			   password.length()<=12 &&
-			   password.matches(".*[0-9]+.*") &&
-			   password.matches(".*[a-z]+.*") &&
-			   password.matches(".*[A-Z]+.*"))
+		if (password.length() >= 8 && password.length() <= 12 && password.matches(".*[0-9]+.*") && password.matches(".*[a-z]+.*") && password.matches(".*[A-Z]+.*"))
 			return true;
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Érvénytelen jelszó", "Érvénytelen jelszó");
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}", "#{out.passwordError}");
 			current.addMessage(null, msg);
 			return false;
 		}
 	}
-	
+
 	private boolean arePasswordsEqual() {
 		if (password.equals(passconf))
 			return true;
-		else{
-			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "A két jelszónak meg kell egyeznie!", "A két jelszónak meg kell egyeznie!");
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "#{out.error}", "#{out.passwordError}");
 			current.addMessage(null, msg);
 			return false;
 		}
