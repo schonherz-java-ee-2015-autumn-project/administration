9151de169172d6530413a4f97bbd16e33310f59b
Viktor Pusinszky <luxeon@hotmail.hu>
2015-12-24 19:16:43 +0100

    add username duplicate validator

--- administration/web/src/main/java/hu/schonherz/administration/web/validator/UserNameEditValidator.java
+++ administration/web/src/main/java/hu/schonherz/administration/web/validator/UserNameEditValidator.java
@@ -3,23 +3,34 @@ package hu.schonherz.administration.web.validator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
 
+import hu.schonherz.administration.serviceapi.UserService;
+import hu.schonherz.administration.serviceapi.dto.UserDTO;
 import hu.schonherz.administration.web.localization.MessageProvider;
 
-@FacesValidator("userNameEditValidator")
+@Named("userNameEditValidator")
+@EJB(name = "ejb.UserService", beanInterface = UserService.class)
+@ViewScoped
 public class UserNameEditValidator implements Validator {
 
+	@EJB
+	UserService userService;
+
+	
 	@Override
-	public void validate(FacesContext arg0, UIComponent arg1, Object arg2) throws ValidatorException {
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
 		
-		
-		String input=(String)arg2;
+		String input = (String) value;
+		UIComponent otherpass = component.findComponent("adminForm:selectId");
+		Long id = (Long) otherpass.getAttributes().get("value");
 		String err = "";
 		if (input.length() < 6)
 			err += MessageProvider.getValue("usernameLengthError");
@@ -31,10 +42,21 @@ public class UserNameEditValidator implements Validator {
 			if (!m.matches())
 				err += MessageProvider.getValue("username_constraint");
 		}
+		if(err.isEmpty()){
+			
+			try {
+				UserDTO user = userService.findUserByName(input);
+				if(user!=null && !(user.getId().equals(id))){
+					err+=MessageProvider.getValue("takenNameError");
+				}
+			} catch (Exception e) {
+			}
+		}
 		if (!err.isEmpty()) {
 			FacesMessage message = new FacesMessage(err);
 			message.setSeverity(FacesMessage.SEVERITY_WARN);
 			throw new ValidatorException(message);
 		}
 	}
+
 }
