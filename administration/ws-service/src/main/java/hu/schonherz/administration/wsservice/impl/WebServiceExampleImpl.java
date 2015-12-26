b1b19ccdccc546b7b9ea958b55a7468a68788496
Viktor Pusinszky <luxeon@hotmail.hu>
2015-12-26 15:13:34 +0100

    works

--- administration/ws-service/src/main/java/hu/schonherz/administration/wsservice/impl/WebServiceExampleImpl.java
+++ administration/ws-service/src/main/java/hu/schonherz/administration/wsservice/impl/WebServiceExampleImpl.java
@@ -1,9 +1,12 @@
 package hu.schonherz.administration.wsservice.impl;
 
 import hu.schonherz.administration.wsserviceapi.WebServiceExample;
+
+import javax.ejb.Stateless;
 import javax.jws.WebService;
 
 
+@Stateless(mappedName = "WebService")
 @WebService(endpointInterface = "hu.schonherz.administration.wsserviceapi.WebServiceExample")
 public class WebServiceExampleImpl implements WebServiceExample {
 
