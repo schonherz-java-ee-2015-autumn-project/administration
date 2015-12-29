c6dbef6014976ffe0eb23e78ebf494e1ec03ecf0
kosimiki <kosimiki@gmail.com>
2015-12-30 00:41:32 +0100

    cargo service interface added

---
+++ administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/RemoteCargoService.java
@@ -0,0 +1,9 @@
+package hu.schonherz.administration.serviceapi;
+
+import hu.schonherz.administration.serviceapi.dto.CargoDTO;
+
+public interface RemoteCargoService {
+	
+	void saveCargo(CargoDTO cargo);
+
+}
