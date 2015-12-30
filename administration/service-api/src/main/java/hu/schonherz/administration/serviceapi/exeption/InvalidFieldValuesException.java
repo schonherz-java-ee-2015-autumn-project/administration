f0d211da47112b1a0b49637a90f830b12f824a9c
kosimiki <kosimiki@gmail.com>
2015-12-30 11:56:55 +0100

    new exception for webservice

---
+++ administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/exeption/InvalidFieldValuesException.java
@@ -0,0 +1,15 @@
+package hu.schonherz.administration.serviceapi.exeption;
+/**
+ * 
+ * @author Miklós Kosárkár
+ * This exceptions is thrown when the required parameter has invalid fields according to documentation.
+ *
+ */
+public class InvalidFieldValuesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7282489612757656918L;
+
+}
