75686c42cb384d6949c06365b21e7105ec49889f
kosimiki <kosimiki@gmail.com>
2015-12-30 12:27:10 +0100

    Added message.

--- administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/exeption/InvalidFieldValuesException.java
+++ administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/exeption/InvalidFieldValuesException.java
@@ -11,5 +11,15 @@ public class InvalidFieldValuesException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = -7282489612757656918L;
-
+	
+	private String errMessage;
+	
+	@Override
+	public String getMessage() {
+		return super.getMessage() + errMessage;
+	}
+	
+	public void setMessage(String message){
+		this.errMessage=message;
+	}
 }
