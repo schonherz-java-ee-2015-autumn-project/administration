938d3bd69c0867e6a07ceeb7a73408d1d67a520a
kosimiki <kosimiki@gmail.com>
2015-12-30 22:01:18 +0100

    unused import removed

--- administration/persistence/src/main/java/hu/schonherz/administration/persistence/entities/Cargo.java
+++ administration/persistence/src/main/java/hu/schonherz/administration/persistence/entities/Cargo.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
