f01edb82ea158e9c180598d67268eba5bdc943db
kosimiki <kosimiki@gmail.com>
2016-01-02 10:27:46 +0100

    Revert "@ApplicationException(rollback=true) added"
    This reverts commit 2f74350f0898bc44b721ca879911ec31b7f5c926.

--- administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/exeption/InvalidFieldValuesException.java
+++ administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/exeption/InvalidFieldValuesException.java
@@ -1,29 +1,25 @@
 package hu.schonherz.administration.serviceapi.exeption;
-
-import javax.ejb.ApplicationException;
-
 /**
  * 
- * @author Miklós Kosárkár This exceptions is thrown when the required parameter
- *         has invalid fields according to documentation.
+ * @author Miklós Kosárkár
+ * This exceptions is thrown when the required parameter has invalid fields according to documentation.
  *
  */
-@ApplicationException(rollback = true)
 public class InvalidFieldValuesException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7282489612757656918L;
-
+	
 	private String errMessage;
-
+	
 	@Override
 	public String getMessage() {
-		return errMessage;
+		return  errMessage;
 	}
-
-	public void setMessage(String message) {
-		this.errMessage = message;
+	
+	public void setMessage(String message){
+		this.errMessage=message;
 	}
 }
