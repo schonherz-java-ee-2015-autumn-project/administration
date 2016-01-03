90de8cbf8616058d5385e603f8d033e9f35c029b
kosimiki <kosimiki@gmail.com>
2016-01-03 17:23:33 +0100

    second id deleted

--- administration/persistence/src/main/java/hu/schonherz/administration/persistence/entities/CourierIncome.java
+++ administration/persistence/src/main/java/hu/schonherz/administration/persistence/entities/CourierIncome.java
@@ -3,18 +3,18 @@ package hu.schonherz.administration.persistence.entities;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class CourierIncome extends BaseEntity implements Serializable  {
+public class CourierIncome extends BaseEntity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4375462293564464326L;
-	
-	private Long id;
+
 	@ManyToOne
 	private User courier;
 	private String courierName;
@@ -25,67 +25,77 @@ public class CourierIncome extends BaseEntity implements Serializable  {
 	private Integer voucher;
 	private Integer actualCash;
 	private Integer actualVoucher;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
+
 	public User getCourier() {
 		return courier;
 	}
+
 	public void setCourier(User courier) {
 		this.courier = courier;
 	}
+
 	public String getCourierName() {
 		return courierName;
 	}
+
 	public void setCourierName(String courierName) {
 		this.courierName = courierName;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public Integer getCash() {
 		return cash;
 	}
+
 	public void setCash(Integer cash) {
 		this.cash = cash;
 	}
+
 	public Integer getSZÉPCard() {
 		return SZÉPCard;
 	}
+
 	public void setSZÉPCard(Integer sZÉPCard) {
 		SZÉPCard = sZÉPCard;
 	}
+
 	public Integer getCrediCard() {
 		return crediCard;
 	}
+
 	public void setCrediCard(Integer crediCard) {
 		this.crediCard = crediCard;
 	}
+
 	public Integer getVoucher() {
 		return voucher;
 	}
+
 	public void setVoucher(Integer voucher) {
 		this.voucher = voucher;
 	}
+
 	public Integer getActualCash() {
 		return actualCash;
 	}
+
 	public void setActualCash(Integer actualCash) {
 		this.actualCash = actualCash;
 	}
+
 	public Integer getActualVoucher() {
 		return actualVoucher;
 	}
+
 	public void setActualVoucher(Integer actualVoucher) {
 		this.actualVoucher = actualVoucher;
 	}
-	
-	
+
 }
