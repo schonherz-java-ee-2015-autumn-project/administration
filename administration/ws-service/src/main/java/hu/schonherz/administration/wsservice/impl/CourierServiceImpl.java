d04d149834d9e19645dfebd238292554bd3afce5
Viktor Pusinszky <luxeon@hotmail.hu>
2016-01-03 23:38:36 +0100

    add new function changePaymentState

--- administration/ws-service/src/main/java/hu/schonherz/administration/wsservice/impl/CourierServiceImpl.java
+++ administration/ws-service/src/main/java/hu/schonherz/administration/wsservice/impl/CourierServiceImpl.java
@@ -10,11 +10,15 @@ import javax.jws.WebService;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
 import hu.schonherz.administration.serviceapi.RemoteCargoService;
+import hu.schonherz.administration.serviceapi.exeption.AddressNotFoundException;
 import hu.schonherz.administration.serviceapi.exeption.BusyCourierException;
 import hu.schonherz.administration.serviceapi.exeption.CargoAlreadyTakenException;
 import hu.schonherz.administration.serviceapi.exeption.CargoNotFoundException;
 import hu.schonherz.administration.serviceapi.exeption.CourierNotFoundException;
+import hu.schonherz.administration.serviceapi.exeption.OrderException;
+import hu.schonherz.administration.wsservice.dto.RemotePaymentMethod;
 import hu.schonherz.administration.wsserviceapi.CourierService;
+import hu.schonherz.administration.wsserviceapi.converter.RemotePaymentConverter;
 
 @Stateless(mappedName = "CourierServiceImpl")
 @WebService(endpointInterface = "hu.schonherz.administration.wsserviceapi.CourierService", serviceName = "CourierServiceImpl")
@@ -29,6 +33,13 @@ public class CourierServiceImpl implements CourierService {
 	public void assignCargoToCourier(long cargoId, long courierId)
 			throws CargoAlreadyTakenException, CargoNotFoundException, CourierNotFoundException, BusyCourierException {
 		cargoService.assignCargoToCourier(cargoId, courierId);
+	}
+
+	@Override
+	public void changePaymentState(Long courierId, Long orderId,RemotePaymentMethod paymentMethod)
+			throws CourierNotFoundException, CargoNotFoundException, OrderException, AddressNotFoundException {
+		cargoService.changePaymentState(courierId,orderId,RemotePaymentConverter.toDTO(paymentMethod));
+		
 	}
 
 }
--- administration/ws-service/src/main/java/hu/schonherz/administration/wsserviceapi/CourierService.java
+++ administration/ws-service/src/main/java/hu/schonherz/administration/wsserviceapi/CourierService.java
@@ -4,10 +4,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import hu.schonherz.administration.serviceapi.exeption.AddressNotFoundException;
 import hu.schonherz.administration.serviceapi.exeption.BusyCourierException;
 import hu.schonherz.administration.serviceapi.exeption.CargoAlreadyTakenException;
 import hu.schonherz.administration.serviceapi.exeption.CargoNotFoundException;
 import hu.schonherz.administration.serviceapi.exeption.CourierNotFoundException;
+import hu.schonherz.administration.serviceapi.exeption.OrderException;
+import hu.schonherz.administration.wsservice.dto.RemotePaymentMethod;
 
 @WebService
 public interface CourierService {
@@ -17,4 +20,8 @@ public interface CourierService {
 			@WebParam(name = "courierID") long courierId) throws CargoAlreadyTakenException, CargoNotFoundException,
 					CourierNotFoundException, BusyCourierException;
 
+	@WebMethod(operationName = "changePaymentState")
+	public void changePaymentState(@WebParam(name = "courierId") Long courierId,
+			@WebParam(name = "orderId") Long orderId, @WebParam(name = "payment") RemotePaymentMethod payment)
+					throws CourierNotFoundException, CargoNotFoundException, OrderException, AddressNotFoundException;
 }
