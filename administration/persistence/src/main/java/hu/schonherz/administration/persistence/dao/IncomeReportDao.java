90b0c405bcc05f4d4c91f8e00462073314412f83
Viktor Pusinszky <luxeon@hotmail.hu>
2016-01-07 23:05:46 +0100

    create new IncomReport entity

---
+++ administration/persistence/src/main/java/hu/schonherz/administration/persistence/dao/IncomeReportDao.java
@@ -0,0 +1,14 @@
+package hu.schonherz.administration.persistence.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import hu.schonherz.administration.persistence.entities.IncomeReport;
+
+@Repository
+public interface IncomeReportDao extends JpaRepository<IncomeReport, Long>, JpaSpecificationExecutor<IncomeReport> {
+
+	IncomeReport findById(long id);
+}
