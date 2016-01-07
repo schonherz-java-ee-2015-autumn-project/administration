163401e0855c166b210e1b2593d6a886660326a2
Viktor Pusinszky <luxeon@hotmail.hu>
2016-01-07 23:23:20 +0100

    Income Report service

---
+++ administration/service-api/src/main/java/hu/schonherz/administration/serviceapi/dto/IncomeReportDTO.java
@@ -0,0 +1,95 @@
+package hu.schonherz.administration.serviceapi.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class IncomeReportDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8684193417925872499L;
+
+	private Long id;
+	private Date date;
+	private Integer creditCard;
+	private Integer szepCard;
+	private Integer voucher;
+	private Integer cash;
+	private Integer totalAmount;
+	private Integer restaurantTransferAmount;
+	private Integer courierServiceAmount;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Integer getCreditCard() {
+		return creditCard;
+	}
+
+	public void setCreditCard(Integer crediCard) {
+		this.creditCard = crediCard;
+	}
+
+	public Integer getSzepCard() {
+		return szepCard;
+	}
+
+	public void setSzepCard(Integer szepCard) {
+		this.szepCard = szepCard;
+	}
+
+	public Integer getVoucher() {
+		return voucher;
+	}
+
+	public void setVoucher(Integer voucher) {
+		this.voucher = voucher;
+	}
+
+	public Integer getCash() {
+		return cash;
+	}
+
+	public void setCash(Integer cash) {
+		this.cash = cash;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(Integer totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Integer getRestaurantTransferAmount() {
+		return restaurantTransferAmount;
+	}
+
+	public void setRestaurantTransferAmount(Integer restaurantTransferAmount) {
+		this.restaurantTransferAmount = restaurantTransferAmount;
+	}
+
+	public Integer getCourierServiceAmount() {
+		return courierServiceAmount;
+	}
+
+	public void setCourierServiceAmount(Integer courierServiceAmount) {
+		this.courierServiceAmount = courierServiceAmount;
+	}
+
+}
