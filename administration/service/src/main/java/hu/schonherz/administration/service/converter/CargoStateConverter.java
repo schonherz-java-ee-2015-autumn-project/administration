f755f832a36c6eb85317f437d7262716e8870a88
Viktor Pusinszky <luxeon@hotmail.hu>
2016-01-20 11:45:17 +0100

    refactor cargo state converter implementation

--- administration/service/src/main/java/hu/schonherz/administration/service/converter/CargoStateConverter.java
+++ administration/service/src/main/java/hu/schonherz/administration/service/converter/CargoStateConverter.java
@@ -1,40 +1,15 @@
 package hu.schonherz.administration.service.converter;
 
-import org.dozer.DozerBeanMapper;
-import org.dozer.Mapper;
-
 import hu.schonherz.administration.persistence.entities.helper.State;
 import hu.schonherz.administration.serviceapi.dto.CargoState;
 
 public class CargoStateConverter {
-	static Mapper mapper = new DozerBeanMapper();
 
-	public static CargoState toDTO(State state) {
-		if (state == null) {
-			return null;
-		}
-		switch(state){
-		case Delivered: return CargoState.Delivered;
-		case Delivering: return CargoState.Delivering;
-		case Free: return CargoState.Free;
-		case Taken: return CargoState.Taken;
-		}
-		return null;
+	public CargoState toDTO(State state) {
+		return CargoState.valueOf(state.name());
 	}
 
-	public static State toEntity(CargoState CargoState) {
-		if (CargoState == null) {
-			return null;
-		}
-		switch(CargoState){
-		case Delivered: return State.Delivered;
-		case Delivering: return State.Delivering;
-		case Free: return State.Free;
-		case Taken: return State.Taken;
-		}
-		return null;
-
+	public State toEntity(CargoState CargoState) {
+		return State.valueOf(CargoState.name());
 	}
-
-
 }
